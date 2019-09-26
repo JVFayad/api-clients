ba2a2b581d95b80022fa9f56138a394901dc144e
JVFayad <joao.victor.fa2014@gmail.com>
2019-09-26 20:49:57 -0300

    chamadas api produto

--- clients/views.py
+++ clients/views.py
@@ -5,10 +5,16 @@ from rest_framework import generics
 
 
 class ClientList(generics.ListCreateAPIView):
+    """
+    List and Create Clients
+    """
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
 
 
 class ClientDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, Update and Delete Clients
+    """
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class ProductList(generics.ListAPIView):
+    """
+    List Products
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetail(generics.RetrieveAPIView):
+    """
+    Retrieve Products
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
